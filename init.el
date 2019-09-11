c88cf88929a5bf25e98d120986c72cc85fe66b76
regbrown99 <reginald.brown@me.com>
2019-09-10 20:42:53 -0500

    Update init.el

--- init.el
+++ init.el
@@ -1,3 +1,8 @@
 ;; My Custom init.el file
 ;; Created: 2019-09-10
 ;; Last updated: 2019-09-10
+
+;; Add the folder to my custom themes to the load path
+;; Allows me to store all of my custom themes in this folder all in one place and have them loaded at Emacs launch.
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+
